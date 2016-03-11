64b702a142036e91ebd4a6211a0a50edb3e6d387
helsayed <hussein.elsayed@badrit.com>
2016-03-11 15:59:28 +0200

    adding test for invalid nil city name

--- spec/models/cities_spec.rb
+++ spec/models/cities_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Cities, type: :model do
       end
     end
 
+    context 'invalid nil city name' do
+      let(:city) { FactoryGirl.build(:city, name: nil) }
+
+      it 'should not be valid' do
+        expect(city.valid?).to eq(false)
+      end
+
+      it 'should have error on name' do
+        expect(city).to  have(1).error_on(:name)
+      end
+    end
 end
