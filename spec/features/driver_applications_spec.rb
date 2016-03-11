7e62b169fae8517f2f1ad86c3aee03c96d1fae1c
helsayed <hussein.elsayed@badrit.com>
2016-03-11 17:34:48 +0200

    add spec for reject driver application

--- spec/features/driver_applications_spec.rb
+++ spec/features/driver_applications_spec.rb
@@ -44,14 +44,14 @@ RSpec.feature "driver_application", :type => :feature do
     expect(page).to have_text("Driver applications list.")
     
     # check driver applications data    
-    find('tr', text: pending_driver_application.first_name).should have_tet('pending')
-    find('tr', text: accepted_driver_application.first_name).should have_tet('accepted')
-    find('tr', text: rejected_driver_application.first_name).should have_tet('rejected')
+    find('tr', text: pending_driver_application.first_name).should have_text('pending')
+    find('tr', text: accepted_driver_application.first_name).should have_text('accepted')
+    find('tr', text: rejected_driver_application.first_name).should have_text('rejected')
   end
 
   scenario "filter by status pending" do 
     select 'pending', from: "status_filter"
-    click_link 'filter'
+    click_button 'filter'
     expect(page).to have_text(pending_driver_application.city.name)
     expect(page).not_to have_text(accepted_driver_application.city.name)
     expect(page).not_to have_text(rejected_driver_application.city.name)
@@ -59,7 +59,7 @@ RSpec.feature "driver_application", :type => :feature do
 
   scenario "filter by status accepted" do 
     select 'pending', from: "status_filter"
-    click_link 'filter'
+    click_button 'filter'
     expect(page).not_to have_text(pending_driver_application.city.name)
     expect(page).to have_text(accepted_driver_application.city.name)
     expect(page).not_to have_text(rejected_driver_application.city.name)
@@ -67,7 +67,7 @@ RSpec.feature "driver_application", :type => :feature do
 
   scenario "filter by status rejected" do 
     select 'pending', from: "status_filter"
-    click_link 'filter'
+    click_button 'filter'
     expect(page).not_to have_text(pending_driver_application.city.name)
     expect(page).not_to have_text(accepted_driver_application.city.name)
     expect(page).to have_text(rejected_driver_application.city.name)
@@ -75,7 +75,7 @@ RSpec.feature "driver_application", :type => :feature do
 
   scenario "filter by city berlin" do 
     select 'Berlin', from: "city_filter"
-    click_link 'filter'
+    click_button 'filter'
     expect(page).to have_text(pending_driver_application.city.name)
     expect(page).not_to have_text(accepted_driver_application.city.name)
     expect(page).not_to have_text(rejected_driver_application.city.name)
@@ -83,7 +83,7 @@ RSpec.feature "driver_application", :type => :feature do
 
   scenario "filter by city cairo" do 
     select 'Cairo', from: "city_filter"
-    click_link 'filter'
+    click_button 'filter'
     expect(page).not_to have_text(pending_driver_application.city.name)
     expect(page).to have_text(accepted_driver_application.city.name)
     expect(page).not_to have_text(rejected_driver_application.city.name)
@@ -91,10 +91,32 @@ RSpec.feature "driver_application", :type => :feature do
 
   scenario "filter by city alex" do 
     select 'Alexandria', from: "city_filter"
-    click_link 'filter'
+    click_button 'filter'
     expect(page).not_to have_text(pending_driver_application.city.name)
     expect(page).not_to have_text(accepted_driver_application.city.name)
     expect(page).to have_text(rejected_driver_application.city.name)
   end
 
+  scenario "accept pending application" do
+    expect(page).to have_text("Driver applications list.")
+    
+    # check driver applications data    
+    find('tr', text: pending_driver_application.first_name).should have_link('accept')
+    find('tr', text: pending_driver_application.first_name).should have_link('reject')
+    click_link 'accept'
+    expect(page).to have_text("Driver application has been approved.")
+    expect(pending_driver_application.status).to eq('accepted')
+  end
+
+
+  scenario "reject pending application" do
+    expect(page).to have_text("Driver applications list.")
+    
+    # check driver applications data    
+    find('tr', text: pending_driver_application.first_name).should have_link('accept')
+    find('tr', text: pending_driver_application.first_name).should have_link('reject')
+    click_link 'reject'
+    expect(page).to have_text("Driver application has been rejected.")
+    expect(pending_driver_application.status).to eq('rejected')
+  end
 end
