50b9c3ec60aac713ce3373a99c0c802972f50ecb
helsayed <hussein.elsayed@badrit.com>
2016-03-11 15:45:09 +0200

    add spec for non valid user first_name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,17 @@ RSpec.describe User, type: :model do
         expect(user.valid?).to eq(true)
       end
     end
+
+    context 'invalid first_name' do
+      let(:user) { FactoryGirl.build(:user, first_name: nil) }
+
+      it 'should not be valid' do
+        expect(user.valid?).to eq(false)
+      end
+
+      it 'should have error on first_name' do
+        expect(user).to  have(1).error_on(:first_name)
+      end
+    end
   end
 end
