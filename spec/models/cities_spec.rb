0eabc5aec4f0144ed98801451f2e73b0cce93087
helsayed <hussein.elsayed@badrit.com>
2016-03-11 15:59:14 +0200

    adding test for valid city creation

--- spec/models/cities_spec.rb
+++ spec/models/cities_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Cities, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'city validation' do
+    context 'valid' do
+      let(:city) { FactoryGirl.build(:city) } # generates a valid city data
+      
+      it 'valid user data' do
+        expect(city.valid?).to eq(true)
+      end
+    end
+
 end
